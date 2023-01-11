95a53d8cc14348471ee02b92b17bf8a199db560d
devguinn <83019267+devguinn@users.noreply.github.com>
2023-01-11 10:52:34 -0500

    Update server.py
    added query params

--- server.py
+++ server.py
@@ -3,19 +3,24 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import os
 
+
+
 def hello_world(request):
-    name = request.args.get('name')
+    name = request.GET.get('name','world')
     if name == None or len(name) == 0:
-        name = os.environ.get('NAME')
-        if name == None or len(name) == 0:
-            name = "world"
+        name = "world"
     message = "Hello, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
+    # port = int(os.environ.get("PORT"))
+    port = os.environ.get("PORT")
+    if port == None:
+        port = 8000
+    port = int(port)
     with Configurator() as config:
         config.add_route('hello', '/')
+        #config.add_route('hello', '/{name}')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
